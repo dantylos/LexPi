9c37e136b8aaf3c0481e9488eafc7ed02ee7fc4d
dantylos <danihme89@gmail.com>
2024-08-21 18:22:10 +0200

    Продолжение

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:0,
-      nav:true,
-      navText: [ '', ' ' ],
-      autoplay:true,
-      autoplayTimeout:2000,
-      responsive:{
-          0:{
-              items:1
-          },
+// $(document).ready(function(){
+//   $('.owl-carousel').owlCarousel({
+//       loop:true,
+//       margin:0,
+//       nav:true,
+//       navText: [ '', ' ' ],
+//       autoplay:true,
+//       autoplayTimeout:2000,
+//       responsive:{
+//           0:{
+//               items:1
+//           },
 
-          1200:{
-              items:6
-          }
-      }
-  });
-}) 
+//           1200:{
+//               items:6
+//           }
+//       }
+//   });
+// }) 
