a6ddd7038167338efc8220873ce70648bc7902f4
dantylos <danihme89@gmail.com>
2024-09-29 21:02:30 +0200

    cources adaptive progress

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ $('.owl-carousel').owlCarousel({
   margin:0,
   nav:true,
   navText: [ '', ' ' ],
-  autoplay:true,
+  autoplay:false,
   autoplayTimeout:10000,
   autoplaySpeed:500,
   autoplayHoverPause:false,
