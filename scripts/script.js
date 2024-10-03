5cbbc75b41491b996f7a61dee1d85e03d26fa51b
dantylos <danihme89@gmail.com>
2024-10-03 14:38:12 +0200

    pages

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ $('.owl-carousel').owlCarousel({
   margin:0,
   nav:true,
   navText: [ '', ' ' ],
-  autoplay:false,
+  autoplay:true,
   autoplayTimeout:10000,
   autoplaySpeed:500,
   autoplayHoverPause:false,
